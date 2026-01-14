031d124ec52d663c6b3e4ae85772de81a4bdf99a
Klaus <thorres@brothersofgrey.net>
2026-01-14 19:40:05 +0100

    Initial commit

---
+++ lib/src/latex_table_base.dart
@@ -0,0 +1,3 @@
+bool isAwesome() {
+  return true;
+}
---
+++ test/latex_table_test.dart
@@ -0,0 +1,14 @@
+import 'package:latex_table/latex_table.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('A group of tests', () {
+    setUp(() {
+      // Additional setup goes here.truetrue
+    });
+
+    test('First Test', () {
+      expect(isAwesome(), isTrue);
+    });
+  });
+}
