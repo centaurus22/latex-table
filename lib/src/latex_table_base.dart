538f1190d70465595fe0abc88f1c5f830c2c0231
Klaus <thorres@brothersofgrey.net>
2026-01-15 22:43:19 +0100

    Implement Exception when no column is defined

--- lib/src/latex_table_base.dart
+++ lib/src/latex_table_base.dart
@@ -2,5 +2,8 @@
 import 'package:latex_table/latex_table.dart';
 
 String generateTable(Table table) {
+  if(table.fieldDefinitions.isEmpty) {
+    throw FormatException('The table needs at least on column.');
+  }
   return '\\begin{tabular}{c}\n\\end{tabular}';
 }
--- test/latex_table_test.dart
+++ test/latex_table_test.dart
@@ -3,10 +3,14 @@ import 'package:test/test.dart';
 
 void main() {
   group('latex generation tests', () {
-    test('generate base table', () {
+    test('generating base table', () {
       var table = Table(fieldDefinitions: [CenteredField()], rows: []);
       var result = '\\begin{tabular}{c}\n\\end{tabular}';
       expect(generateTable(table), result);
+    });
+    test('whether error is thrown when no field is defined', () {
+      var table = Table(fieldDefinitions: [], rows: []);
+      expect(()=> generateTable(table), throwsFormatException);
     });
   });
 }
