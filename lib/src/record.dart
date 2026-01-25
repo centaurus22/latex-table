c6350400cda90ae7de75f6195267a094479438dd
Klaus <thorres@brothersofgrey.net>
2026-01-25 14:53:21 +0100

    Add doc comments  to the records

--- lib/src/record.dart
+++ lib/src/record.dart
@@ -1,49 +1,139 @@
+/// The starting point of the structure that will be converted.
+/// 
+/// It contains the data and the structure of the latex table.
 class Table {
+  /// A list of column definitions.
+  /// 
+  /// A [ColumnDefinition] defines if a column is left aligned, centered
+  /// or right aligned.
   final List<ColumnDefinition> columnDefinitions;
+
+  /// A list of rows.
+  /// 
+  /// Rows can contain either data fields or simply represent a horizontal
+  /// line in various forms.
   final List<Row> rows;
+
+  /// Requires the [columnDefinitions] and the [rows].
+  ///
+  /// The parser returns an [Error] if the [columnDefinitions] list is empty.
   Table({required this.columnDefinitions, required this.rows});
 }
 
+/// The definition of one column.
+/// 
+/// This is the sealed base class. It can either be a [LeftAlignedColumn],
+/// a [CenteredColumn] or a [RightAlignedColumn].
 sealed class ColumnDefinition {}
 
+/// This represents a column which content is aligned to the left.
 class LeftAlignedColumn extends ColumnDefinition {}
 
+/// This represents a column which content is centered.
 class CenteredColumn extends ColumnDefinition {}
 
+/// This represents a column which content is aligned to the right.
 class RightAlignedColumn extends ColumnDefinition {}
 
+/// This represents a row in the table.
+/// 
+/// This is the sealed base class.
+/// It can either be a [DataRow] that contains [Field]s with information
+/// or header, or a [TopRule], [MidRule] or [BottomRule] for the various
+/// styles of horizontal lines in scientific tables.
 sealed class Row {}
 
+/// The horizontal line at the top of a scientific table.
+/// 
+/// The sealed base class is the [Row]. Other row-like objects are
+/// the [DataRow] that contains [Field]s with data or header, or a [MidRule]
+/// or [BottomRule] for the other styles of horizontal lines in a scientific
+/// tables.
 class TopRule extends Row {}
 
+/// The horizontal line inside a scientific table.
+/// 
+/// The sealed base class is the [Row]. Other row-like objects are the [DataRow]
+/// that contains [Field]s with data or header, or a [TopRule] or [BottomRule]
+///  for the other styles of horizontal lines in a scientific tables.
 class MidRule extends Row {}
 
+/// The horizontal line at the bottom of a scientific table.
+/// 
+/// The sealed base class is the [Row]. Other row-like objects are the [DataRow]
+/// that contains [Field]s with data or header, or a [TopRule] or [MidRule]
+/// for the other styles of horizontal lines in a scientific tables.
 class BottomRule extends Row {}
 
+/// A row of table data or header.
+/// 
+/// The sealed base class is the [Row]. Other rows or the [TopRule] [MidRule]
+/// and [BottomRule] for the various styles of horizontal lines in
+/// a scientific tables.
 class DataRow extends Row {
+  /// A [List] of [Field]s in one row.
+  /// 
+  /// The list contains the data or headers of a table.
   final List<Field> fields;
+  
+  /// This requires the [fields].
   DataRow({required this.fields});
 }
 
+/// This contains a table datum or header.
+/// 
+/// A special [Field] is the [EuroField].
 class Field {
+  /// The value of the field.
   final String value;
+
+  /// This requires the [value] of the [Field].
   Field({required this.value});
 }
 
+/// This [Field] is for currency values in Euro.
+/// 
+/// In the generated Latex table this value will be used as a parameter for
+/// the EUR command from the eurosym package for LATEX. This ads a € symbol
+/// to your value together with a unbreakable thin space in between.
+/// 
+/// The base object [Field] contains just normal data or header.
 class EuroField extends Field {
+  /// This requires the [value] of the [Field].
   EuroField({required super.value});
 }
 
+/// A result of a function which can be a [Success] or an [Error].
+/// 
+/// This is the sealed base class. Its children are used in functions that can
+/// return an [Error] instead of a of an expected value ([Success])
+/// or additional [warnings].
 sealed class Result {
+  /// A list of warnings.
   final List<String> warnings = [];
 }
 
+/// This is returned when functions are executed correctly.
+///
+/// It can contain additional [warnings].
+/// The sealed base class is the [Result]. An [Error] can be returned too.
 class Success extends Result {
+  /// The embedded value.
   final String value;
+
+  /// This requires the embedded [value].
   Success({required this.value});
 }
 
+/// This is returned when a parameter is not valid.
+///
+/// It can contain additional [warnings].
+/// The sealed base class ist the [Result]. A [Success]] can be returned too
+/// when the function executed correctly.
 class Error extends Result {
+  /// The error message.
   final String message;
+  
+  /// This requires the error [message].
   Error({required this.message});
 }
