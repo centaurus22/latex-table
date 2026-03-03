c51557a81ff9e3a595fce1e7c2554e7966dac814
Klaus <thorres@brothersofgrey.net>
2026-03-03 18:16:05 +0100

    Add information to library export file

--- lib/latex_table.dart
+++ lib/latex_table.dart
@@ -1,9 +1,23 @@
-/// Support for doing something awesome.
+/// A Dart library to parse an object structure to LaTeX source code for a simple
+/// table.
 ///
-/// More dartdocs go here.
+/// The object structure can be easily generated programmatically.
+/// 
+/// Features:
+/// * Supports left aligned, right aligned and centered columns.
+/// * Generates LaTeX sources for a scientific table: If the first row is rule,
+///   it is converted to a `\toprule`. If the last row is a rule it is converted
+///   to a `\bottomrule`. Every other rule is converted to a `\midrule`.
+/// * Supports a special table field for values ​​with currency specifications in
+///   Euros.
+/// * Returns an error if the LaTeX parser won't compile the LaTeX source,
+/// * Returns warnings if the number of columns differs from the number of
+///   fields in a data row.
+/// * And last but not least: Columns are nicely aligned :)
+///
+/// If you need any feature please [contact](#contact) me.
+
 library;
 
 export 'src/latex_table_base.dart';
 export 'src/record.dart';
-
-// TODO: Export any libraries intended for clients of this package.
